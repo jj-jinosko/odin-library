1be0c154c2e4e32a9de2750128348e3e6bbbeb71
jj-jinosko <jesscj@umich.edu>
2024-02-15 17:29:20 -0500

    add book card form

--- script.js
+++ script.js
@@ -7,17 +7,24 @@ function Book(title, author, quote, read) {
   this.read = read;
 }
 
+// const book1 = new Book(
+//     "Island of the Blue Dolphins",
+//     "Scott O'Dell",
+//     "TBD",
+//     true,
+// );
+
 const book1 = new Book(
-    "Island of the Blue Dolphins",
-    "Scott O'Dell",
-    "TBD",
+    "Braiding Sweetgrass", 
+    "Robin Wall-Kimmerer",
+    "Action on behalf of life transforms. Because the relationship between self and the world is reciprocal, it is not a question of first getting enlightened or saved and then acting. As we work to heal the earth, the earth heals us.",
     true,
-);
+)
 
 const book2 = {
     title: "The Andromeda Strain ",
     author: "Micahel Crichton",
-    quote: "TBD",
+    quote: "The rock, for its part, is not even aware of our existence because we are alive for only a brief instant of its lifespan. To it, we are like flashes in the dark.",
     read: true,
 }
 
@@ -27,6 +34,7 @@ const book3 = new Book(
     "There is one kind of prison where the man is behind bars, and everything that he desires is outside; and there is another kind where the things are behind the bars, and the man is outside., They were trying to save their souls- and who but a fool could fail to see that all that was the matter with their souls was that they had not been able to get a decent existence for their bodies?",
     true,
 )
+
 
 const myLibrary = [];
 const bookshelf = document.querySelector(".bookshelf");
@@ -42,10 +50,28 @@ myLibrary.push(book1, book2);
 
 // display book
 function displayBook(bookNum) {
-        console.log("test");
         let book = document.createElement("div");
-        bookshelf.appendChild(book);
-        book.innerText = myLibrary[bookNum].title;
+        let bookTitle = document.createElement("div");
+        let bookAuthor = document.createElement("div");
+        let bookQuote = document.createElement("div");
+        let bookRead = document.createElement("div");
+
+        book.setAttribute("class", "book");
+        bookTitle.setAttribute("class", "title");
+        bookAuthor.setAttribute("class", "author");
+        bookQuote.setAttribute("class", "quote");
+        bookRead.setAttribute("class", "read");
+
+        bookshelf.prepend(book); // make new book the first one displayed after the input form
+        book.appendChild(bookTitle);
+        book.appendChild(bookAuthor);
+        book.appendChild(bookQuote);
+        book.appendChild(bookRead);
+        
+        bookTitle.innerText = myLibrary[bookNum].title;
+        bookAuthor.innerText = myLibrary[bookNum].author;
+        bookQuote.innerText = myLibrary[bookNum].quote;
+        bookRead.innerText = myLibrary[bookNum].read;
 }
 
 // load existing books when window opens
@@ -56,12 +82,37 @@ window.addEventListener("load", () => {
     }
   });
 
+// make new book and add to library
+
+function makeBook(){
+    // let newBook = document.getElementById("newTitle");
+    let newTitle = document.getElementById("newTitle").value;
+    let newAuthor = document.getElementById("newAuthor").value;
+    let newQuote = document.getElementById("newQuote").value;
+    let newRead = document.getElementById("newRead").value;
+    let newBook = new Book(
+        newTitle,
+        newAuthor,
+        newQuote,
+        newRead,
+    )
+    return newBook;
+}
+
+const addBookBtn = document.querySelector(".addBookBtn");
+addBookBtn.addEventListener("click", (e) => {
+    e.preventDefault(); // prevent submit button from reloading pg bc no data is actually getting back to server
+    addBookToLibrary(makeBook());
+    displayBook(myLibrary.length - 1);
+    document.getElementById("bookForm").reset();
+})
+
+// test button
 
 const testButton = document.querySelector(".pushMe");
 testButton.addEventListener("click", () => { 
     addBookToLibrary(book3);
     displayBook(myLibrary.length - 1);
 });
-
 
 
